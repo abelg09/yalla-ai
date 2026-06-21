88cdd6de0eb55f6acfac10298e1915afcb70d6d4
abelg09 <abel@metawareit.com>
2026-06-21 18:08:38 +0400

    Fix demo image rendering

--- README.md
+++
@@ -1,5 +0,0 @@
-# Yalla AI
-
-Live app: https://abelg09.github.io/yalla-ai/
-
-Latest deploy: investor demo UX QA, local demo assets, mobile polish, and demo-mode flows.
--- assets/label-BngwXxd4.js
+++
@@ -1,1 +0,0 @@
-import{r,j as t}from"./react-CakZar-y.js";import{J as a}from"./radix-ui-DIG4j3jl.js";import{b as m,m as i}from"./index-DDyhoBTT.js";const l=i("text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"),p=r.forwardRef(({className:s,...e},o)=>t.jsx(a,{ref:o,className:m(l(),s),...e}));p.displayName=a.displayName;export{p as L};
